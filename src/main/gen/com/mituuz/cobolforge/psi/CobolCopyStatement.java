94ff8d872fb37d629a5719cbb5467f719bfd12bb
Mitja <mitja@mituuz.com>
2026-01-30 17:45:24 +0200

    Fix missing method from bnf

--- src/main/gen/com/mituuz/cobolforge/psi/CobolCopyStatement.java
+++ src/main/gen/com/mituuz/cobolforge/psi/CobolCopyStatement.java
@@ -4,9 +4,12 @@ package com.mituuz.cobolforge.psi;
 import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
 
 public interface CobolCopyStatement extends PsiElement {
 
   @Nullable PsiElement getIdentifier();
+
+  @Nullable PsiReference getReference();
 
 }
